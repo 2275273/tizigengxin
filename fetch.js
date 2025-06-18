4d6667ccabf8364f137a5c03743afa28c38b5489
2275273 <68908577+2275273@users.noreply.github.com>
2025-06-19 07:18:29 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -5,8 +5,8 @@ async function fetchContent() {
   const res = await fetch('https://tizi.239000.xyz/download/sub');
   const text = await res.text();
 
-  // 注意文件名就是 "index"，没有后缀
-  fs.writeFileSync('index', text, 'utf-8');
+  // 注意：这里写到 index.html，但内容是纯文本，没有 HTML 标签
+  fs.writeFileSync('index.html', text, 'utf-8');
 }
 
 fetchContent();
